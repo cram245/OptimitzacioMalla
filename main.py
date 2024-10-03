dc2d0aa21b6d8402b727e9b969dc25ed7f898e8b
cram245 <marcalbaf3@gmail.com>
2024-10-03 12:18:46 +0200

    afegit calcul de l'error

--- main.py
+++ main.py
@@ -38,14 +38,16 @@ def calculaJacobiana(y, T):
     
     return jacobiana
 
-def newtonRaphson(y0, T, tol=1e-7, max_iter=100):
+def newtonRaphson(y0, T, tol=5e-8, max_iter=100):
     y = y0
+    errors = []
     for i in range(max_iter):
         residu = calculaResidu(y, T)
         
+
         if np.linalg.norm(residu) < tol:
             print(f'Convergencia alcanzada en {i} iteraciones')
-            return y
+            return y, errors
         jacobiana = calculaJacobiana(y, T)
 
         if i == 0:
@@ -53,19 +55,30 @@ def newtonRaphson(y0, T, tol=1e-7, max_iter=100):
             print('Jacobiana 0,0: ', jacobiana[0][0])
 
         delta_y = np.linalg.solve(jacobiana, -residu)
+        errors.append(np.log10(np.linalg.norm(-delta_y)/np.linalg.norm(y + delta_y)))
         y = y + delta_y
+        
     print('No se alcanzó la convergencia')
-    return y
+    return y, errors
 
 y = coordsToDofs(X)  # Convertir las coordenadas iniciales a vector de incógnitas
-y_opt = newtonRaphson(y, T)
+y_opt, errors = newtonRaphson(y, T)
+plt.plot(errors)
+plt.title('Gràfica de Convergència')
+plt.xlabel('Iteració')
+plt.ylabel('Log(Error)')
 
+plt.grid(True)
+plt.show()
 
 
 # Convert you vector with the dofs back to mesh coordinates
 X = dofsToCoords(y_opt)
 print(calculaDistorsioMalla(X, T))
 
-plotMesh(X, 'Segon plot')
+plotMesh(X, 'Solució')
 X = dofsToCoords(np.ones(y_opt.shape))
+
+
+    
 
